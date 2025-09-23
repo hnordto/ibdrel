5aba14c124ba3b8d1f25c17d5f2729c384dafb2f
Henrik Nordtorp <105918296+hnordto@users.noreply.github.com>
2025-09-23 12:52:57 +0200

    Remove old scripts for Shiny input and output functions

--- inst/shiny/R/input.R
+++
@@ -1,7 +0,0 @@
-shinyInput <- function(FUN, len, id, ...) {
-  inputs <- character(len)
-  for (i in seq_len(len)) {
-    inputs[i] <- as.character(FUN(paste0(id, i), label = NULL, ...))
-  }
-  inputs
-}
