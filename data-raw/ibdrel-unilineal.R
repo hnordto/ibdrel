049b63995ef3f5952ea8a228aa5ab4749322280a
Henrik Nordtorp <105918296+hnordto@users.noreply.github.com>
2026-03-03 16:56:52 +0100

    Update ibdrel default unilieal model

--- data-raw/ibdrel-unilineal.R
+++ data-raw/ibdrel-unilineal.R
@@ -3,8 +3,8 @@ library(ibdrel)
 library(dplyr)
 
 l = identify_path_lengths(0,11)
-rels = listRelationships(l)
-rels <- removeSymmetries(rels)
+rels = listRelationships(l, ignoreSex=T)
+#rels <- removeSymmetries(rels)
 
 # Define "close" relationships: Where relationships with all combinations of sex paths should be present
 close.rels <- c(1,2,3)
@@ -13,12 +13,18 @@ rels_close <- rels |> filter(degree %in% close.rels)
 rels_distant <- rels |> filter(!(degree %in% close.rels))
 
 # Only keep full maternal/paternal lineages for "distant" relationships
-rels_paternal <- rels_distant |> group_by(type, degree, removal, nGen, cousDeg, half) |>
-  slice_head(n = 1)
-rels_maternal <- rels_distant |> group_by(type, degree, removal, nGen, cousDeg, half) |>
-  slice_tail(n = 1)
+#rels_paternal <- rels_distant |> group_by(type, degree, removal, nGen, cousDeg, half) |>
+#  slice_head(n = 1)
+#rels_maternal <- rels_distant |> group_by(type, degree, removal, nGen, cousDeg, half) |>
+#  slice_tail(n = 1)
 
-rels <- rbind(rels_close, rels_paternal, rels_maternal)
+peds_close <- constructPedigrees(rels_close)
+peds_distant <- constructPedigrees(rels_distant)
+
+rels_close_variants = do.call(rbind.data.frame, lapply(peds_close, pedVariants))
+rels_distant_variants = do.call(rbind.data.frame, lapply(peds_distant, pedVariants, onlyExtremes = TRUE))
+
+rels <- rbind(rels_close_variants, rels_distant_variants)
 peds <- constructPedigrees(rels)
 
 annotation = sapply(peds, annotatePedigree)
@@ -30,7 +36,9 @@ metadata = pedsMetadata(peds)
 
 # SIMULATE TRAINING DATA
 
-sims_training <- ibdSimulations(peds, N = 1000, seed = NULL)
+seed = seq(1,length(peds))+20260303
+
+sims_training <- ibdSimulations(peds, N = 1000, seed = seed)
 segments <- lengthIBD(sims_training, peds, annotation)
 
 ibdrel_unilineal <- list(
